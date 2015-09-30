a88fb3f912bada07509e8b1bf55981861c00ab61
Austin Sprague <Sprague@Austins-MacBook-Pro.local>
2015-09-30 14:45:50 -0700

    error correction

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 var question1 = prompt('Was I born in SF?');
 
-if (question1.toUpperCase()==='YES' ) {
+if (question1.toUpperCase()==='YES' || question1.toUpperCase()==='Y' ) {
 	alert('Correct, I was born in SF');
 } else {
 	alert('Incorrect, I was born in SF');
 } 
 
-var question2 = prompt('Is my favorite food Pizza?');
+var question2 = prompt('Is my favorite food Pizza?' );
 
-if (question2.toUpperCase()==='YES' ){
+if (question2.toUpperCase()==='YES' || question2.toUpperCase()==='Y' ){
 	alert('Correct, Pizza is great.');
 } else {
 	alert('Incorrect, Pizza is the best.');
@@ -16,7 +16,7 @@ if (question2.toUpperCase()==='YES' ){
 
 var question3 = prompt('Do I dip my fries in chocolate shakes?');
 
-if (question3.toUpperCase()==='YES' ) {
+if (question3.toUpperCase()==='YES' || question3.toUpperCase()==='Y') {
 	alert('Correct, thank you for the intro Wendy\'s!');
 } else {
 	alert('It\'s wierd, but I do.');
