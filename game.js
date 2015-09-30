f4bd46bbe0a787447dbb9eddd83fbd01af7efd70
Austin Sprague <Sprague@Austins-MacBook-Pro.local>
2015-09-30 14:02:39 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,11 @@
+var question1 = prompt("Was I born in SF?");
+var question2 = prompt("Is my favorite food Pizza?");
+var question3 = prompt("Do I like to dip my fries in chocolate shakes?")
+
+console.log("Question 1 response:" + question1);
+console.log("Question 2 response:" + question2);
+console.log("Question 3 response:" + question3);
+
+alert("Your answer to Question 1 was " + question1);
+alert("Your answer to Question 2 was " + question2);
+alert("Your answer to Question 3 was " + question3);
