d60626e7b7420ea9e99f73b833add59d368384d0
samjarzembowski <sjarzembowski@gmail.com>
2015-10-01 14:01:51 -0700

    moved questions into functions and called those functions

--- game.js
+++ game.js
@@ -1,31 +1,39 @@
 var correctAnswers = 0;
-var question1 = prompt('Was I born in SF?');
 
-
-if (question1.toUpperCase()==='YES' || question1.toUpperCase()==='Y') {
-	alert('Correct, I was born in SF');
-	correctAnswers++;
-} else {
-	alert('Incorrect, I was born in SF');
-} 
-
-var question2 = prompt('Is my favorite food Pizza?');
-
-if (question2.toUpperCase()==='YES' || question2.toUpperCase()==='Y' ){
-	alert('Correct, Pizza is great.');
-	correctAnswers++;
-} else {
-	alert('Incorrect, Pizza is the best.');
+function ques1() {
+  var question1 = prompt('Was I born in SF?');
+  if (question1.toUpperCase()==='YES' || question1.toUpperCase()==='Y') {
+  	alert('Correct, I was born in SF');
+  	correctAnswers++;
+  } else {
+  	alert('Incorrect, I was born in SF');
+  }
 }
 
-var question3 = prompt('Do I dip my fries in chocolate shakes?');
-
-if (question3.toUpperCase()==='YES' || question3.toUpperCase()==='Y') {
-	alert('Correct, thank you for the intro Wendy\'s!');
-	correctAnswers++;
-} else {
-	alert('It\'s wierd, but I do.');
+function ques2() {
+  var question2 = prompt('Is my favorite food Pizza?');
+  if (question2.toUpperCase()==='YES' || question2.toUpperCase()==='Y' ){
+  	alert('Correct, Pizza is great.');
+  	correctAnswers++;
+  } else {
+  	alert('Incorrect, Pizza is the best.');
+  }
 }
+
+function ques3() {
+  var question3 = prompt('Do I dip my fries in chocolate shakes?');
+  if (question3.toUpperCase()==='YES' || question3.toUpperCase()==='Y') {
+  	alert('Correct, thank you for the intro Wendy\'s!');
+  	correctAnswers++;
+  } else {
+  	alert('It\'s wierd, but I do.');
+  }
+}
+
+
+ques1();
+ques2();
+ques3();
 
 console.log('User got ' + correctAnswers + ' correct answers.');
 
