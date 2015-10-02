3c1d967b61eee8e67c9040dc19b59e8a58a423f7
Sabrina Tee <sabrinatee@me.com>
2015-10-02 14:27:40 -0700

    Code style update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-10/01/2015: Sam Jarzembowski: Driver, Austin Sprague: Navigator
+10/01/2015: Sam Jarzembowski: Driver, Austin Sprague: Navigator.
 
-10/02/2015: Sabrina Tee: Driver, Austin Sprague: Navigator
+10/02/2015: Sabrina Tee: Driver, Austin Sprague: Navigator.
--- game.js
+++ game.js
@@ -5,19 +5,18 @@ var four = document.getElementById('four');
 
 var correctAnswers = 0;
 
-var questions = ["Was I born in SF?", "Is my favorite food Pizza?", "Do I dip my fries in chocolate shakes?"]
+var questions = ["Was I born in SF?", "Is my favorite food Pizza?", "Do I dip my fries in chocolate shakes?"];
 var answers = ["YES", "Y", "NO", "N"];
 
 function ques1() {
   var question1 = prompt(questions[0]);
   if (question1.toUpperCase()=== answers[0] || question1.toUpperCase()===answers[1]) {
   	one.className = 'correct';
-    one.innerHTML = '<p>Correct, I was born in SF.</p>'+'<p><img src="img/dudeomg.jpg" width="200" height="200"/></p>';
+    one.innerHTML = 'Correct, I was born in SF.'+'<p><img src="img/dudeomg.jpg" width="200" height="200"/></p>';
     correctAnswers++;
-
   } else {
   	one.className = 'incorrect';
-    one.innerHTML = 'Incorrect, I was born in SF';
+    one.innerHTML = 'Incorrect, I was born in SF.';
   }
 }
 
@@ -25,7 +24,7 @@ function ques2() {
   var question2 = prompt(questions[1]);
   if (question2.toUpperCase()===answers[0] || question2.toUpperCase()===answers[1]){
   	two.className = 'correct';
-    two.innerHTML = '<p>Correct, Pizza is great.</p>'+'<p><img src="img/dudeomg.jpg" width="200" height="200"/></p>';
+    two.innerHTML = 'Correct, Pizza is great.'+'<p><img src="img/dudeomg.jpg" width="200" height="200"/></p>';
     correctAnswers++;
   } else {
   	two.className = 'incorrect';
@@ -37,7 +36,7 @@ function ques3() {
   var question3 = prompt(questions[2]);
   if (question3.toUpperCase()===answers[0] || question3.toUpperCase()===answers[1]) {
   	three.className = 'correct';
-    three.innerHTML = '<p>Correct, thank you for the intro Wendy\'s!</p>'+'<p><img src="img/dudeomg.jpg" width="200" height="200"/></p>';
+    three.innerHTML = 'Correct, thank you for the intro Wendy\'s!'+'<p><img src="img/dudeomg.jpg" width="200" height="200"/></p>';
     correctAnswers++;
   } else {
     three.className = 'incorrect';
